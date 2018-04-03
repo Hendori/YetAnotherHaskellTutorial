3f746ffc41ec23b5868b096ac7ed1a1c7e0be4a2
Hendri Eleveld <hendrieleveld@Hendris-Air.fritz.box>
2018-04-03 07:20:54 +0200

    added exercises 3.7, 3.8 and 3.9

--- Hoofdstuk3/exercises.hs
+++ Hoofdstuk3/exercises.hs
@@ -7,3 +7,16 @@ answer34 xs = length (filter Data.Char.isLower xs)
 answer35 xs = foldr max 0 xs
 
 answer36 xs = fst (head (tail xs))
+
+-- Exercise 3.7
+fib 0 = 1
+fib 1 = 1
+fib n = (fib (n-1))+(fib (n-2))
+
+-- Exercise 3.8
+multi a 1 = a
+multi a b = a + (multi a (b-1))
+
+-- Exercise 3.9
+my_map f [] = []
+my_map f (x:xs) = f x: my_map f xs
